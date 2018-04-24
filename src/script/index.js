d5e8de585aef2cb86461ac84f1f6684ba998566b
Calhoun5 <johnhckuo@gmail.com>
2018-04-24 10:07:44 +0800

    enable hashHistory

--- src/script/index.js
+++ src/script/index.js
@@ -1,16 +1,15 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, hashHistory } from 'react-router-dom'
 
 import Routes from "./components/route/main"
 import "../style/reset.css"
 import "../style/global.css"
 
 const app = document.getElementById('app')
-const BaseURL = "/";
 
 ReactDOM.render(
-	<BrowserRouter basename={BaseURL}>
+	<BrowserRouter history={hashHistory}>
 		<Routes />
 	</BrowserRouter>
  , app);
