8adf9adb80fa8108baf832d96db18b6b81cb757b
Calhoun5 <johnhckuo@gmail.com>
2018-04-24 10:21:03 +0800

    hashRouter -> Router

--- src/script/index.js
+++ src/script/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import { BrowserRouter, hashHistory } from 'react-router-dom'
+import { Router } from 'react-router-dom'
 
 import Routes from "./components/route/main"
 import "../style/reset.css"
@@ -9,7 +9,7 @@ import "../style/global.css"
 const app = document.getElementById('app')
 
 ReactDOM.render(
-	<BrowserRouter history={hashHistory}>
+	<Router>
 		<Routes />
-	</BrowserRouter>
+	</Router>
  , app);
